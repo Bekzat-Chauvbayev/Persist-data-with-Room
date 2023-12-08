d09dd6937fbf2af31c0a45e3be20273e008a42f7
Bekzat-Chauvbayev <kazna51k@gmail.com>
2023-12-08 19:45:41 +0600

    update item delete item sell item finally

--- app/src/main/java/com/example/inventory/ui/item/ItemDetailsViewModel.kt
+++ app/src/main/java/com/example/inventory/ui/item/ItemDetailsViewModel.kt
@@ -25,18 +25,32 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 /**
  * ViewModel to retrieve, update and delete an item from the [ItemsRepository]'s data source.
  */
 class ItemDetailsViewModel(
     savedStateHandle: SavedStateHandle,
-    itemsRepository: ItemsRepository
+    val itemsRepository: ItemsRepository
 ) : ViewModel() {
+
+    fun reduceQuantityByOne() {
+        viewModelScope.launch {
+            val currentItem =uiState.value.itemDetails.toItem()
+            if(currentItem.quantity>0){
+                itemsRepository.updateItem(currentItem.copy(quantity = currentItem.quantity-1))
+            }
+        }
+    }
+
+    suspend fun deleteItem(){
+        itemsRepository.deleteItem(uiState.value.itemDetails.toItem())
+    }
 
     private val itemId: Int = checkNotNull(savedStateHandle[ItemDetailsDestination.itemIdArg])
     val uiState: StateFlow<ItemDetailsUiState> = itemsRepository.getItemStream(itemId).filterNotNull().map {
-        ItemDetailsUiState( itemDetails = it.toItemDetails())
+        ItemDetailsUiState(outOfStock = it.quantity<=0, itemDetails = it.toItemDetails())
 
      }.stateIn(scope = viewModelScope, started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS), initialValue = ItemDetailsUiState())
     companion object {
